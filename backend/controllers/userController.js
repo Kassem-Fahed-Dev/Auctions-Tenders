f6c3932cb2386fc4c245d03098ce4c06864b7ba4
Akroom234js <112288110+Akroom234js@users.noreply.github.com>
2025-03-07 21:33:53 +0300

    add two routes with funcitons (updateuser and delete user)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,39 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     },
   });
 });
+
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ name: req.params.name });
+  if (user) {
+      User.deleteOne({ name: req.params.name })
+          .exec()
+          .then((result) => {
+              res.status(200).json({ message: 'User deleted' });
+          })
+          .catch((err) => {
+              console.log(err);
+              res.status(500).json({ error: err });
+          });
+  } else {
+      console.log('User not exist');
+      res.status(500).json({ message: 'User not exist' });
+  }
+});
+exports.updateInfo = async (req, res, next) => {
+  const userId = req.params.id;
+  const updateData = req.body;
+  User.findByIdAndUpdate(userId, updateData, { new: true })
+      .exec()
+      .then((updatedUser) => {
+          if (!updatedUser) {
+              return res.status(404).json({ message: 'User not found' });
+          }
+          res.status(200).json({ message: 'User info updatd', user: updatedUser });
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json({ error: err });
+      });
+};
+
--- backend/routs/userRoutes.js
+++ backend/routs/userRoutes.js
@@ -4,9 +4,9 @@ const userController = require('./../controllers/userController.js');
 const router = express.Router();
 
 router.get('/', userController.getAllUsers);
-router.post('/singup', authController.singUp);
+router.post('/signup', authController.singUp);
 router.post('/login', authController.login);
-
+router.delete('/:name', userController.deleteUser).put('/:id', userController.updateInfo);
 router.post('/forgotPassword', authController.forgetPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
