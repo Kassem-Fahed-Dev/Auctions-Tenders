2588d2745358cdc08014d4d58f4d4a1421c761bb
Akroom234js <112288110+Akroom234js@users.noreply.github.com>
2025-03-17 11:14:42 +0300

    add some CRUD operations

--- backend/routs/auctionRoutes.js
+++ backend/routs/auctionRoutes.js
@@ -3,5 +3,7 @@ const auctionController = require('../controllers/auctionController');
 const router = express.Router();
 
 router.post('/createAuction', auctionController.addAuction);
+router.delete('/:id', auctionController.deleteAuction); // id-> AuctionId
+router.get('/:id', auctionController.getAllUserAuctions); // id-> UserId
 
 module.exports = router;
