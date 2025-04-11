dd07bae321a76aad79d564a10a16828908895055
hasanngh <hhasannjjgg@gmail.com>
2025-04-11 23:54:31 +0300

    details

--- frontend/src/commponts/Auctions/Auction.js
+++ frontend/src/commponts/Auctions/Auction.js
@@ -1,15 +1,15 @@
 import im4 from '../../image/car.jpg';
 import im5 from '../../image/alarm.png';
+import { Link } from 'react-router-dom';
 export default function Auction() {
-   const x='جاري'
+  const x = 'جاري';
   return (
-   
     <div className="oneAuction">
-      <p className={x=='جاري'?'time':'time1'}>جاري</p>
-      <button className='far fa-heart'></button>
-      <img className="imageAuction" src={im4} />
+      <p className={x === 'جاري' ? 'time' : 'time1'}>جاري</p>
+      <button className="far fa-heart"></button>
+      <img className="imageAuction" src={im4} alt="Error" />
       <p className="nameCube">غسالة حوضين للبيع</p>
-      
+
       <div className="userName">
         <i className="fas fa-user userAuction"></i>
         <div>الناشر: هديل</div>
@@ -22,7 +22,9 @@ export default function Auction() {
         <div className="product1 product2">
           <div>المدة 4 يوم</div>
           {/* <i className="fa fa-clock clockAuction"></i> */}
-          <div className='clockAuction'><img  src={im5}/></div>
+          <div className="clockAuction">
+            <img src={im5} alt="Error" />
+          </div>
         </div>
 
         <div className="product1">
@@ -32,19 +34,22 @@ export default function Auction() {
           </div>
           <i className="far fa-clock clockAuction"></i>
         </div>
-     
-      <div className="product1">
+
+        <div className="product1">
           <div>
             <div>2025/03/10 </div>
-            <div>2025/03/10  </div>{' '}
+            <div>2025/03/10 </div>{' '}
           </div>
           <i className="far fa-calendar-alt clockAuction"></i>
         </div>
-        </div>
-        <hr></hr>
-        <div className="many">سعر الافتتاح : <span>250
-            </span> مليون ليرة سورية.</div>
-            <button className="ditales">التفاصيل</button>
+      </div>
+      <hr></hr>
+      <div className="many">
+        سعر الافتتاح : <span>250</span> مليون ليرة سورية.
+      </div>
+      <Link to="/det" className="ditales">
+        التفاصيل
+      </Link>
     </div>
   );
 }
