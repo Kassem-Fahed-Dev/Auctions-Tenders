4ab5e60e6de70633ec8e0e5a253ea4e077e79398
HadeelAl-hasan <hadeelalhasan000@gmail.com>
2025-04-28 15:39:54 -0700

    no-profile

--- frontend/src/commponts/Account/Profile/store/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ptnReducer from "../store/Redux";
-export const Store = configureStore({
-  reducer: {
-    ptn_edit: ptnReducer,
-  },
-});
