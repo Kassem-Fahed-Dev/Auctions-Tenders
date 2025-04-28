d8dee89c9377b7ed8ca771b38572d6d2d7aec04e
Kassem-Fahed-Dev <kassemfahed2003@gmail.com>
2025-04-28 21:18:37 +0300

    fixed route '/me'

--- backend/routs/userRoutes.js
+++ backend/routs/userRoutes.js
@@ -3,29 +3,37 @@ const authController = require('./../controllers/authController.js');
 const userController = require('./../controllers/userController.js');
 const router = express.Router();
 
-router.get('/' ,userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/logout', authController.protect,authController.logout);
-router.get('/checkLogin',authController.protect,authController.checkLogin);
-
+router.get('/logout', authController.protect, authController.logout);
+router.get('/checkLogin', authController.protect, authController.checkLogin);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/checkResetCode', authController.checkResetCode);
 router.patch('/resetPassword', authController.resetPassword);
-router.get('/:id',userController.getUser)
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser,
+);
 
 //router.use(authController.protect);
 
-router.patch('/updateMyPassword', authController.protect,authController.updatePassword);
-router.get('/me', authController.protect,userController.getMe, userController.getUser);
-router.patch('/updateMe', authController.protect,userController.updateMe);
-router.delete('/deleteMe', authController.protect,userController.deleteMe);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 //router.use(authController.restrictTo('admin'));
 
 router
   .route('/:id')
+  .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
 
