390f50af87e6f1c4276db4734a26ba2ca872e076
HadeelAl-hasan <hadeelalhasan000@gmail.com>
2025-09-07 20:05:52 -0700

    add

--- frontend/src/commponts/Account/details/Details.js
+++ frontend/src/commponts/Account/details/Details.js
@@ -166,7 +166,7 @@ export default function Details() {
               </div>
               <div>
                 السعر الحالي:
-                <span>{data?.startingPrice}</span>
+                <span>{data?.highestPrice}</span>
               </div>
               <div>
                 خطوة المزايدة :<span>{data?.minimumIncrement}</span>
